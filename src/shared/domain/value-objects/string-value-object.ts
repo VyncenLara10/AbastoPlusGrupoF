eae420cdf5d0f0af4b5ba497917249c4b81b3c64
R.Guti <124416087+RaulvGuti@users.noreply.github.com>
2026-02-17 09:17:13 -0600

    String-Value
    Codigo dentro de string-onjet

---
+++ src/shared/domain/value-objects/string-value-object.ts
@@ -0,0 +1,13 @@
+abstract class StringValueObject {
+    private readonly value: string;
+    constructor(value: string) {
+        this.ensureValueIsValid(value);
+        this.value = value;
+    }
+
+    private ensureValueIsValid(value: string): void {
+        if (typeof value !== 'string' || value.trim().length === 0) {
+            throw new Error(`${this.constructor.name} debe ser una cadena de texto valida y no estar vacia.`);
+        }
+    }
+}
