b999336e910dcdda89f86f41bfefadaf1fe2f3c9
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2026-02-19 09:10:54 -0600

    fix productId VO

--- src/catalog/product/domain/value-objects/product-id.ts
+++ src/catalog/product/domain/value-objects/product-id.ts
@@ -3,12 +3,6 @@ import IdentifierValueObject from '../../../../shared/domain/value-objects/ident
 export default class ProductId extends IdentifierValueObject {
     constructor(value: string) {
         super(value);
-    }
-
-    ensureValueIsUvid(value: string): void {
-        const uvidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-        if (!uvidRegex.test(value)) {
-            throw new Error('Value must be a valid UVID');
-        }
+        super.ensureValueIsUvid(value);
     }
 }
