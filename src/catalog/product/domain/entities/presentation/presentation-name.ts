193bc2ad3c7b9ac0c4c1b07769f8c23db2f4f248
Vyncen <vyncen.lara@gmail.com>
2026-02-19 09:48:16 -0600

    Create: presentation-id presentation-name

--- src/catalog/product/domain/entities/presentation/presentation-name.ts
+++ src/catalog/product/domain/entities/presentation/presentation-name.ts
@@ -1,1 +1,18 @@
-"StringVO"
+import StringValueObject from '../../../../../shared/domain/value-objects/string-value-object';
+
+export default class PresetnationName extends StringValueObject {
+    constructor(value: string) {
+        super(value);
+        this.ensureValueIsValid(value);
+    }
+
+    ensureValueIsValid(value: string): void {
+        super.ensureValueIsValid(value);
+        if (value.length < 10) {
+            throw new Error(`${this.constructor.name} no puede tener mmenos de 10 caracteres.`);
+        }
+    }
+    public getValue(): string {
+        return this.value;
+    }
+}
