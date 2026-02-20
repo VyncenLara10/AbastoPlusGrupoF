e3c8d3666c3ccf272c86c04cc7786a8d835abe87
R.Guti <124416087+RaulvGuti@users.noreply.github.com>
2026-02-19 22:04:22 -0600

    Present
    Se hizo el presentation-net

--- src/catalog/product/domain/entities/presentation/presentation-net-quantity.ts
+++ src/catalog/product/domain/entities/presentation/presentation-net-quantity.ts
@@ -1,1 +1,22 @@
-"NumberVO"
+import IntValueObject from '../../../../../shared/domain/value-objects/int-value-object';
+
+export default class PresentationNetQuantity extends IntValueObject {
+    constructor(value: string) {
+        super(value);
+        this.ensureValueIsValid(value);
+    }
+
+    public ensureValueIsValid(value: string): void {
+        const numericValue = parseInt(value);
+        if (numericValue < 0) {
+            throw new Error(`${this.constructor.name} no puede ser una cantidad negativa.`);
+        }
+        if (numericValue === 0) {
+            throw new Error(`${this.constructor.name} debe ser mayor a cero.`);
+        }
+    }
+
+    public getValue(): number {
+        return parseInt(this.value);
+    }
+}
