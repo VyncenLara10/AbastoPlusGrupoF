8c6297f0cf99d19692c316224060c484e2b080e5
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2026-02-26 09:51:04 -0600

    fix id value object

--- src/shared/domain/value-objects/identifier-value-object.ts
+++ src/shared/domain/value-objects/identifier-value-object.ts
@@ -6,9 +6,14 @@ export default class IdentifierValueObject extends ValueObject<string> {
         this.ensureValueIsUvid(value);
     }
 
+    numberToUUID(num: number): string {
+        const hex = num.toString(16).padStart(32, "0");
+        return `${hex.substring(0, 8)}-${hex.substring(8, 12)}-${hex.substring(12, 16)}-${hex.substring(16, 20)}-${hex.substring(20)}`;
+    }
+
     ensureValueIsUvid(value: string): void {
         const uvidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-        if (!uvidRegex.test(value)) {
+        if (!uvidRegex.test(this.numberToUUID(parseInt(value)))) {
             throw new Error('Value must be a valid UVID');
         }
     }
